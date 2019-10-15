3b5c40ac901fbac7fb43e0405aa40debdebb243a
Tanner Gooding <tagoo@outlook.com>
2019-10-14 17:50:07 -0700

    Regenerating the um/d3dcompiler.h bindings for Windows 10.0.18362.0

--- sources/Interop/D3DCompiler/um/d3dcompiler/D3DCOMPILER_STRIP_FLAGS.cs
+++ sources/Interop/D3DCompiler/um/d3dcompiler/D3DCOMPILER_STRIP_FLAGS.cs
@@ -1,6 +1,6 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 
-// Ported from um\d3dcompiler.h in the Windows SDK for Windows 10.0.15063.0
+// Ported from um/d3d12video.h in the Windows SDK for Windows 10.0.18362.0
 // Original source is Copyright © Microsoft. All rights reserved.
 
 using System;
@@ -11,15 +11,10 @@ namespace TerraFX.Interop
     public enum D3DCOMPILER_STRIP_FLAGS
     {
         D3DCOMPILER_STRIP_REFLECTION_DATA = 0x00000001,
-
         D3DCOMPILER_STRIP_DEBUG_INFO = 0x00000002,
-
         D3DCOMPILER_STRIP_TEST_BLOBS = 0x00000004,
-
         D3DCOMPILER_STRIP_PRIVATE_DATA = 0x00000008,
-
         D3DCOMPILER_STRIP_ROOT_SIGNATURE = 0x00000010,
-
-        D3DCOMPILER_STRIP_FORCE_DWORD = 0x7FFFFFFF
+        D3DCOMPILER_STRIP_FORCE_DWORD = 0x7FFFFFFF,
     }
 }
