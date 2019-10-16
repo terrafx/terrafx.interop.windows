61ae3a73c38bdbfb265c29c9486dc5963a111236
Tanner Gooding <tagoo@outlook.com>
2019-10-15 21:13:41 -0700

    Regenerating the Interop.Windows bindings for Windows 10.0.18362.0

--- sources/Interop/Windows/shared/wtypes/DECIMAL.cs
+++ sources/Interop/Windows/shared/wtypes/DECIMAL.cs
@@ -1,6 +1,6 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 
-// Ported from shared\WTypesbase.h in the Windows SDK for Windows 10.0.15063.0
+// Ported from shared/wtypes.h in the Windows SDK for Windows 10.0.18362.0
 // Original source is Copyright © Microsoft. All rights reserved.
 
 using System.Runtime.InteropServices;
@@ -11,17 +11,20 @@ namespace TerraFX.Interop
     {
         public ushort wReserved;
 
+        [NativeTypeName("DECIMAL::(anonymous union at shared/wtypes.h:697:5)")]
         public _Anonymous1_e__Union Anonymous1;
 
         [NativeTypeName("ULONG")]
         public uint Hi32;
 
+        [NativeTypeName("DECIMAL::(anonymous union at shared/wtypes.h:705:5)")]
         public _Anonymous2_e__Union Anonymous2;
 
         [StructLayout(LayoutKind.Explicit)]
         public partial struct _Anonymous1_e__Union
         {
             [FieldOffset(0)]
+            [NativeTypeName("struct (anonymous struct at shared/wtypes.h:698:9)")]
             _Anonymous_e__Struct Anonymous;
 
             [FieldOffset(0)]
@@ -42,6 +45,7 @@ namespace TerraFX.Interop
         public partial struct _Anonymous2_e__Union
         {
             [FieldOffset(0)]
+            [NativeTypeName("struct (anonymous struct at shared/wtypes.h:706:9)")]
             public _Anonymous_e__Struct Anonymous;
 
             [FieldOffset(0)]
