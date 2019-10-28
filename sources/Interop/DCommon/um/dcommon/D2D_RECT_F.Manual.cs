60be9b3097a0201517d66b0848d4392574e139c2
Nathan Alden, Sr <1189759+nathan-alden-sr@users.noreply.github.com>
2019-10-28 17:47:31 -0400

    Convenience constructors for some basic structs

---
+++ sources/Interop/DCommon/um/dcommon/D2D_RECT_F.Manual.cs
@@ -0,0 +1,18 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/dcommon.h in the Windows SDK for Windows 10.0.18362.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+namespace TerraFX.Interop
+{
+    public partial struct D2D_RECT_F
+    {
+        public D2D_RECT_F(float left, float top, float right, float bottom)
+        {
+            this.left = left;
+            this.top = top;
+            this.right = right;
+            this.bottom = bottom;
+        }
+    }
+}
