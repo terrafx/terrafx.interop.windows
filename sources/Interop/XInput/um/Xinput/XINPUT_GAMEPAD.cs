fc546abedd5788f9696b22e8d9619c2a6cf6a7d2
Tanner Gooding <tagoo@outlook.com>
2020-03-09 17:28:38 -0700

    Generating bindings for um/Xinput.h from Windows SDK for Windows 10.0.18362.0

---
+++ sources/Interop/XInput/um/Xinput/XINPUT_GAMEPAD.cs
@@ -0,0 +1,31 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/Xinput.h in the Windows SDK for Windows 10.0.18362.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+namespace TerraFX.Interop
+{
+    public partial struct XINPUT_GAMEPAD
+    {
+        [NativeTypeName("WORD")]
+        public ushort wButtons;
+
+        [NativeTypeName("BYTE")]
+        public byte bLeftTrigger;
+
+        [NativeTypeName("BYTE")]
+        public byte bRightTrigger;
+
+        [NativeTypeName("SHORT")]
+        public short sThumbLX;
+
+        [NativeTypeName("SHORT")]
+        public short sThumbLY;
+
+        [NativeTypeName("SHORT")]
+        public short sThumbRX;
+
+        [NativeTypeName("SHORT")]
+        public short sThumbRY;
+    }
+}
