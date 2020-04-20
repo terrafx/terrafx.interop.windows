0e90dbcb691d96e5fbf7d2891a0718c185b15fa5
Nathan Alden, Sr <1189759+nathan-alden-sr@users.noreply.github.com>
2020-04-20 11:19:14 -0400

    Generating SoftPub bindings

---
+++ sources/Interop/WinTrust/um/SoftPub/WTD_GENERIC_CHAIN_POLICY_DATA.cs
@@ -0,0 +1,40 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/SoftPub.h in the Windows SDK for Windows 10.0.18362.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace TerraFX.Interop
+{
+    public unsafe partial struct WTD_GENERIC_CHAIN_POLICY_DATA
+    {
+        [NativeTypeName("_WTD_GENERIC_CHAIN_POLICY_DATA::(anonymous union at C:/Program Files (x86)/Windows Kits/10/Include/10.0.18362.0/um/SoftPub.h:169:5)")]
+        public _Anonymous_e__Union Anonymous;
+
+        [NativeTypeName("PWTD_GENERIC_CHAIN_POLICY_CREATE_INFO")]
+        public WTD_GENERIC_CHAIN_POLICY_CREATE_INFO* pSignerChainInfo;
+
+        [NativeTypeName("PWTD_GENERIC_CHAIN_POLICY_CREATE_INFO")]
+        public WTD_GENERIC_CHAIN_POLICY_CREATE_INFO* pCounterSignerChainInfo;
+
+        [NativeTypeName("PFN_WTD_GENERIC_CHAIN_POLICY_CALLBACK")]
+        public IntPtr* pfnPolicyCallback;
+
+        [NativeTypeName("void *")]
+        public void* pvPolicyArg;
+
+        [StructLayout(LayoutKind.Explicit)]
+        public partial struct _Anonymous_e__Union
+        {
+            [FieldOffset(0)]
+            [NativeTypeName("DWORD")]
+            public uint cbStruct;
+
+            [FieldOffset(0)]
+            [NativeTypeName("DWORD")]
+            public uint cbSize;
+        }
+    }
+}
