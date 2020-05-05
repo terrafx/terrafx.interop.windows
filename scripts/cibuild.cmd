dc89fa674dec0669a63254d16be0ef5e95cfb860
Ingmar Bitter <ingmar.bitter@gmail.com>
2020-05-04 22:47:41 -0400

    cibuild.cmd typo fix refs #46

--- scripts/cibuild.cmd
+++ scripts/cibuild.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0build.ps1""" -ci %*"
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0build.ps1""" -ci -solution """%~dpo0..\samples\GuiSamples.sln""" %*"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0build.ps1""" -ci -solution """%~dp0..\samples\GuiSamples.sln""" %*"
 EXIT /B %ERRORLEVEL%
