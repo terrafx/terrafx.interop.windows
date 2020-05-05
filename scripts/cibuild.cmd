0b7d58ddaba0215bddf91269f187aec5949adbe9
Ingmar Bitter <ingmar.bitter@gmail.com>
2020-05-04 22:26:30 -0400

    New .sln to not affect the build scripts of the main solution file. cibuild.cmd updated to build new GuiSamples.sln refs #46

--- scripts/cibuild.cmd
+++ scripts/cibuild.cmd
@@ -1,3 +1,4 @@
 @ECHO OFF
 powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0build.ps1""" -ci %*"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0build.ps1""" -ci -solution """%~dpo0..\samples\GuiSamples.sln""" %*"
 EXIT /B %ERRORLEVEL%
