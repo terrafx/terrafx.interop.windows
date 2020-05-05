b9ea195c044a0a595f54f90c153e0557dadfbdb0
Nathan Alden, Sr <1189759+nathan-alden-sr@users.noreply.github.com>
2020-05-05 11:24:38 -0400

    Corrected case of kernel32 library path

--- sources/Interop/Advapi32/Advapi32.cs
+++ sources/Interop/Advapi32/Advapi32.cs
@@ -4,6 +4,6 @@ namespace TerraFX.Interop
 {
     public static unsafe partial class Advapi32
     {
-        private const string LibraryPath = "Kernel32";
+        private const string LibraryPath = "kernel32";
     }
 }
