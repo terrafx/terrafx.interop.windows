f88c34fca8205ae1859b52ca330ec5f695ea8779
Ingmar Bitter <ingmar.bitter@gmail.com>
2020-05-18 22:36:20 -0400

    Added HelloConstBuffer form C++

--- samples/DirectX/Program.cs
+++ samples/DirectX/Program.cs
@@ -14,6 +14,7 @@ namespace TerraFX.Samples.DirectX
         private static readonly DXSample[] Samples = {
             new HelloWindow11(1280, 720, "D3D11.HelloWindow"),
             new HelloTriangle11(1280, 720, "D3D11.HelloTriangle"),
+            new HelloTriangle12(1280, 720, "D3D12.HelloConstBuffer"),
             new HelloWindow12(1280, 720, "D3D12.HelloWindow"),
             new HelloTriangle12(1280, 720, "D3D12.HelloTriangle")
         };
