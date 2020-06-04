8ea414cf73ac86f31c2a26d1adafdbe67735b809
Tanner Gooding <tagoo@outlook.com>
2020-06-04 16:40:00 -0700

    Adding a THIRD-PARTY-NOTICES and adjusting the header for the um/dxcapi and other/dxc/dxcapi files

--- sources/Interop/Windows/um/dxcapi/Windows.Manual.cs
+++ sources/Interop/Windows/um/dxcapi/Windows.Manual.cs
@@ -1,3 +1,8 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/dxcapi.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved. Licensed under the University of Illinois Open Source License.
+
 using System;
 using System.Runtime.InteropServices;
 
