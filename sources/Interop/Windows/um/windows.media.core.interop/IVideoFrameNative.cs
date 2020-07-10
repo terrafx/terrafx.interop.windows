3369d71ac0b22d945a5cbcd059b6eaa1a2dfd99f
reflectronic <john-tur@outlook.com>
2020-07-10 00:56:38 -0400

    Add windows.media.core.interop.h

---
+++ sources/Interop/Windows/um/windows.media.core.interop/IVideoFrameNative.cs
@@ -0,0 +1,65 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/windows.media.core.interop.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace TerraFX.Interop
+{
+    [Guid("26BA702B-314A-4620-AAF6-7A51AA58FA18")]
+    public unsafe partial struct IVideoFrameNative
+    {
+        public void** lpVtbl;
+
+        [return: NativeTypeName("HRESULT")]
+        public int QueryInterface([NativeTypeName("const IID &")] Guid* riid, [NativeTypeName("void **")] void** ppvObject)
+        {
+            return ((delegate* stdcall<IVideoFrameNative*, Guid*, void**, int>)(lpVtbl[0]))((IVideoFrameNative*)Unsafe.AsPointer(ref this), riid, ppvObject);
+        }
+
+        [return: NativeTypeName("ULONG")]
+        public uint AddRef()
+        {
+            return ((delegate* stdcall<IVideoFrameNative*, uint>)(lpVtbl[1]))((IVideoFrameNative*)Unsafe.AsPointer(ref this));
+        }
+
+        [return: NativeTypeName("ULONG")]
+        public uint Release()
+        {
+            return ((delegate* stdcall<IVideoFrameNative*, uint>)(lpVtbl[2]))((IVideoFrameNative*)Unsafe.AsPointer(ref this));
+        }
+
+        [return: NativeTypeName("HRESULT")]
+        public int GetIids([NativeTypeName("ULONG *")] uint* iidCount, [NativeTypeName("IID **")] Guid** iids)
+        {
+            return ((delegate* stdcall<IVideoFrameNative*, uint*, Guid**, int>)(lpVtbl[3]))((IVideoFrameNative*)Unsafe.AsPointer(ref this), iidCount, iids);
+        }
+
+        [return: NativeTypeName("HRESULT")]
+        public int GetRuntimeClassName([NativeTypeName("HSTRING *")] IntPtr* className)
+        {
+            return ((delegate* stdcall<IVideoFrameNative*, IntPtr*, int>)(lpVtbl[4]))((IVideoFrameNative*)Unsafe.AsPointer(ref this), className);
+        }
+
+        [return: NativeTypeName("HRESULT")]
+        public int GetTrustLevel([NativeTypeName("TrustLevel *")] TrustLevel* trustLevel)
+        {
+            return ((delegate* stdcall<IVideoFrameNative*, TrustLevel*, int>)(lpVtbl[5]))((IVideoFrameNative*)Unsafe.AsPointer(ref this), trustLevel);
+        }
+
+        [return: NativeTypeName("HRESULT")]
+        public int GetData([NativeTypeName("const IID &")] Guid* riid, [NativeTypeName("LPVOID *")] void** ppv)
+        {
+            return ((delegate* stdcall<IVideoFrameNative*, Guid*, void**, int>)(lpVtbl[6]))((IVideoFrameNative*)Unsafe.AsPointer(ref this), riid, ppv);
+        }
+
+        [return: NativeTypeName("HRESULT")]
+        public int GetDevice([NativeTypeName("const IID &")] Guid* riid, [NativeTypeName("LPVOID *")] void** ppv)
+        {
+            return ((delegate* stdcall<IVideoFrameNative*, Guid*, void**, int>)(lpVtbl[7]))((IVideoFrameNative*)Unsafe.AsPointer(ref this), riid, ppv);
+        }
+    }
+}
