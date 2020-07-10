2db7de354b891465b4628a46665889a31349a703
reflectronic <john-tur@outlook.com>
2020-07-10 15:44:40 -0400

    Add windows.graphics.directx.direct3d11.interop.h

---
+++ sources/Interop/Windows/um/windows.graphics.directx.direct3d11.interop/Windows.cs
@@ -0,0 +1,20 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/windows.graphics.directx.direct3d11.interop.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+using System.Runtime.InteropServices;
+
+namespace TerraFX.Interop
+{
+    public static unsafe partial class Windows
+    {
+        [DllImport("D3D11", EntryPoint = "CreateDirect3D11DeviceFromDXGIDevice", ExactSpelling = true)]
+        [return: NativeTypeName("HRESULT")]
+        public static extern int CreateDirect3D11DeviceFromDXGIDevice([NativeTypeName("IDXGIDevice *")] IDXGIDevice* dxgiDevice, [NativeTypeName("IInspectable **")] IInspectable** graphicsDevice);
+
+        [DllImport("D3D11", EntryPoint = "CreateDirect3D11SurfaceFromDXGISurface", ExactSpelling = true)]
+        [return: NativeTypeName("HRESULT")]
+        public static extern int CreateDirect3D11SurfaceFromDXGISurface([NativeTypeName("IDXGISurface *")] IDXGISurface* dgxiSurface, [NativeTypeName("IInspectable **")] IInspectable** graphicsSurface);
+    }
+}
