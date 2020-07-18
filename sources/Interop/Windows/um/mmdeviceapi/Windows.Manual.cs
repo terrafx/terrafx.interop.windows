8e0416f40fa1cb7b88e2cf49a7a5b5bbe39a284a
reflectronic <john-tur@outlook.com>
2020-07-18 04:20:01 -0400

    Add missing externs

--- sources/Interop/Windows/um/mmdeviceapi/Windows.Manual.cs
+++ sources/Interop/Windows/um/mmdeviceapi/Windows.Manual.cs
@@ -10,8 +10,10 @@ namespace TerraFX.Interop
 {
     public static unsafe partial class Windows
     {
+        public static readonly Guid CLSID_MMDeviceEnumerator = new Guid(0XBCDE0395, 0XE52F, 0X467C, 0X8E, 0X3D, 0XC4, 0X57, 0X92, 0X91, 0X69, 0X2E);
+
         public static readonly Guid IID_IActivateAudioInterfaceAsyncOperation = new Guid(0x72A22D78, 0xCDE4, 0x431D, 0xB8, 0xCC, 0x84, 0x3A, 0x71, 0x19, 0x9B, 0x6D);
-    
+
         public static readonly Guid IID_IActivateAudioInterfaceCompletionHandler = new Guid(0x41D949AB, 0x9862, 0x444A, 0x80, 0xF6, 0xC2, 0x61, 0x33, 0x4D, 0xA5, 0xEB);
 
         public static readonly Guid IID_IMMDevice = new Guid(0xD666063F, 0x1587, 0x4E43, 0x81, 0xF1, 0xB9, 0x48, 0xE8, 0x07, 0x36, 0x3F);
@@ -25,5 +27,7 @@ namespace TerraFX.Interop
         public static readonly Guid IID_IMMEndpoint = new Guid(0x1BE09788, 0x6894, 0x4089, 0x85, 0x86, 0x9A, 0x2A, 0x6C, 0x26, 0x5A, 0xC5);
 
         public static readonly Guid IID_IMMNotificationClient = new Guid(0x7991EEC9, 0x7E89, 0x4D85, 0x83, 0x90, 0x6C, 0x70, 0x3C, 0xEC, 0x60, 0xC0); 
+
+        public static readonly Guid LIBID_MMDeviceAPILib = new Guid(0X2FDAAFA3, 0X7523, 0X4F66, 0X99, 0X57, 0X9D, 0X5E, 0X7F, 0XE6, 0X98, 0XF6);
     }
 }
