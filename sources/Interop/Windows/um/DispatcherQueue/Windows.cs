20301198aa13b3fd551d032d29ae954957e1b15a
reflectronic <john-tur@outlook.com>
2020-08-22 21:25:10 -0400

    Add DispatcherQueue.h

---
+++ sources/Interop/Windows/um/DispatcherQueue/Windows.cs
@@ -0,0 +1,16 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/DispatcherQueue.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+using System.Runtime.InteropServices;
+
+namespace TerraFX.Interop
+{
+    public static unsafe partial class Windows
+    {
+        [DllImport("CoreMessaging", ExactSpelling = true)]
+        [return: NativeTypeName("HRESULT")]
+        public static extern int CreateDispatcherQueueController(DispatcherQueueOptions options, [NativeTypeName("ABI::Windows::System::IDispatcherQueueController **")] void** dispatcherQueueController);
+    }
+}
