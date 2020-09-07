323ca7524b0364006c65cc2e715014bb9e03ba87
reflectronic <john-tur@outlook.com>
2020-09-07 18:08:47 -0400

    Add flags attributes

--- sources/Interop/Windows/um/directmanipulation/DIRECTMANIPULATION_GESTURE_CONFIGURATION.cs
+++ sources/Interop/Windows/um/directmanipulation/DIRECTMANIPULATION_GESTURE_CONFIGURATION.cs
@@ -3,8 +3,11 @@
 // Ported from um/directmanipulation.h in the Windows SDK for Windows 10.0.19041.0
 // Original source is Copyright © Microsoft. All rights reserved.
 
+using System;
+
 namespace TerraFX.Interop
 {
+    [Flags]
     public enum DIRECTMANIPULATION_GESTURE_CONFIGURATION
     {
         DIRECTMANIPULATION_GESTURE_NONE = 0,
