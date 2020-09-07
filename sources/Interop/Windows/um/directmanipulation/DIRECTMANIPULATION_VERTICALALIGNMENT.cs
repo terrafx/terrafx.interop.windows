79c995861acece89d50d91f5ad9f72bbacbc5438
reflectronic <john-tur@outlook.com>
2020-09-07 17:45:55 -0400

    Add directmanipulation.h

---
+++ sources/Interop/Windows/um/directmanipulation/DIRECTMANIPULATION_VERTICALALIGNMENT.cs
@@ -0,0 +1,16 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/directmanipulation.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+namespace TerraFX.Interop
+{
+    public enum DIRECTMANIPULATION_VERTICALALIGNMENT
+    {
+        DIRECTMANIPULATION_VERTICALALIGNMENT_NONE = 0,
+        DIRECTMANIPULATION_VERTICALALIGNMENT_TOP = 0x1,
+        DIRECTMANIPULATION_VERTICALALIGNMENT_CENTER = 0x2,
+        DIRECTMANIPULATION_VERTICALALIGNMENT_BOTTOM = 0x4,
+        DIRECTMANIPULATION_VERTICALALIGNMENT_UNLOCKCENTER = 0x8,
+    }
+}
