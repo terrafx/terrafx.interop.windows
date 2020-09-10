b5c28ffa9bcaa9ad0eb468cfc7b875d107519bf7
reflectronic <john-tur@outlook.com>
2020-09-09 20:02:32 -0400

    Add missing LIBID

---
+++ sources/Interop/Windows/um/inkrenderer/Windows.Manual.cs
@@ -0,0 +1,14 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/InkPresenterDesktop.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+using System;
+
+namespace TerraFX.Interop
+{
+    public static partial class Windows
+    {
+        public static readonly Guid LIBID_InkD2DRendererLib = new Guid(0x390D0AB0, 0x19E2, 0x46BB, 0x86, 0x2E, 0xB0, 0x9F, 0x3C, 0xDC, 0xF8, 0xB9);
+    }
+}
