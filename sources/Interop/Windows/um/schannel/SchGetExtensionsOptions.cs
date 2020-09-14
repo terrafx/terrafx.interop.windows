6d031d38e6e3e5b88baa1e7f6e50010fbd9737b8
reflectronic <john-tur@outlook.com>
2020-09-14 15:19:40 -0400

    Add flags attribute

--- sources/Interop/Windows/um/schannel/SchGetExtensionsOptions.cs
+++ sources/Interop/Windows/um/schannel/SchGetExtensionsOptions.cs
@@ -3,8 +3,11 @@
 // Ported from um/schannel.h in the Windows SDK for Windows 10.0.19041.0
 // Original source is Copyright © Microsoft. All rights reserved.
 
+using System;
+
 namespace TerraFX.Interop
 {
+    [Flags]
     public enum SchGetExtensionsOptions
     {
         SCH_EXTENSIONS_OPTIONS_NONE = 0x0,
