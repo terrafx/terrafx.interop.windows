211eb789afc0e40fc1192cb97c80b0cfd7b4836f
reflectronic <john-tur@outlook.com>
2020-09-27 14:40:57 -0400

    Add RadialControllerInterop.h

---
+++ sources/Interop/Windows/um/RadialControllerInterop/IRadialControllerInterop.cs
@@ -0,0 +1,67 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/RadialControllerInterop.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace TerraFX.Interop
+{
+    [Guid("1B0535C9-57AD-45C1-9D79-AD5C34360513")]
+    [NativeTypeName("struct IRadialControllerInterop : IInspectable")]
+    public unsafe partial struct IRadialControllerInterop
+    {
+        public void** lpVtbl;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [return: NativeTypeName("HRESULT")]
+        public int QueryInterface([NativeTypeName("const IID &")] Guid* riid, [NativeTypeName("void **")] void** ppvObject)
+        {
+            return ((delegate* unmanaged<IRadialControllerInterop*, Guid*, void**, int>)(lpVtbl[0]))((IRadialControllerInterop*)Unsafe.AsPointer(ref this), riid, ppvObject);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [return: NativeTypeName("ULONG")]
+        public uint AddRef()
+        {
+            return ((delegate* unmanaged<IRadialControllerInterop*, uint>)(lpVtbl[1]))((IRadialControllerInterop*)Unsafe.AsPointer(ref this));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [return: NativeTypeName("ULONG")]
+        public uint Release()
+        {
+            return ((delegate* unmanaged<IRadialControllerInterop*, uint>)(lpVtbl[2]))((IRadialControllerInterop*)Unsafe.AsPointer(ref this));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [return: NativeTypeName("HRESULT")]
+        public int GetIids([NativeTypeName("ULONG *")] uint* iidCount, [NativeTypeName("IID **")] Guid** iids)
+        {
+            return ((delegate* unmanaged<IRadialControllerInterop*, uint*, Guid**, int>)(lpVtbl[3]))((IRadialControllerInterop*)Unsafe.AsPointer(ref this), iidCount, iids);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [return: NativeTypeName("HRESULT")]
+        public int GetRuntimeClassName([NativeTypeName("HSTRING *")] IntPtr* className)
+        {
+            return ((delegate* unmanaged<IRadialControllerInterop*, IntPtr*, int>)(lpVtbl[4]))((IRadialControllerInterop*)Unsafe.AsPointer(ref this), className);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [return: NativeTypeName("HRESULT")]
+        public int GetTrustLevel([NativeTypeName("TrustLevel *")] TrustLevel* trustLevel)
+        {
+            return ((delegate* unmanaged<IRadialControllerInterop*, TrustLevel*, int>)(lpVtbl[5]))((IRadialControllerInterop*)Unsafe.AsPointer(ref this), trustLevel);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [return: NativeTypeName("HRESULT")]
+        public int CreateForWindow([NativeTypeName("HWND")] IntPtr hwnd, [NativeTypeName("const IID &")] Guid* riid, [NativeTypeName("void **")] void** ppv)
+        {
+            return ((delegate* unmanaged<IRadialControllerInterop*, IntPtr, Guid*, void**, int>)(lpVtbl[6]))((IRadialControllerInterop*)Unsafe.AsPointer(ref this), hwnd, riid, ppv);
+        }
+    }
+}
