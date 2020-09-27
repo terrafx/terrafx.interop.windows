e42ba3e0872aac491666fc6aa520df54b4c6acdc
reflectronic <john-tur@outlook.com>
2020-09-27 13:47:44 -0400

    Add remotesystemadditionalinfo.h

---
+++ generation/um/remotesystemadditionalinfo/um-remotesystemadditionalinfo.h
@@ -0,0 +1,2 @@
+#include <Windows.h>
+#include <remotesystemadditionalinfo.h>
---
+++ sources/Interop/Windows/um/remotesystemadditionalinfo/IRemoteSystemAdditionalInfoProvider.cs
@@ -0,0 +1,46 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/remotesystemadditionalinfo.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace TerraFX.Interop
+{
+    [Guid("EEAA3D5F-EC63-4D27-AF38-E86B1D7292CB")]
+    [NativeTypeName("struct IRemoteSystemAdditionalInfoProvider : IUnknown")]
+    public unsafe partial struct IRemoteSystemAdditionalInfoProvider
+    {
+        public void** lpVtbl;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [return: NativeTypeName("HRESULT")]
+        public int QueryInterface([NativeTypeName("const IID &")] Guid* riid, [NativeTypeName("void **")] void** ppvObject)
+        {
+            return ((delegate* unmanaged<IRemoteSystemAdditionalInfoProvider*, Guid*, void**, int>)(lpVtbl[0]))((IRemoteSystemAdditionalInfoProvider*)Unsafe.AsPointer(ref this), riid, ppvObject);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [return: NativeTypeName("ULONG")]
+        public uint AddRef()
+        {
+            return ((delegate* unmanaged<IRemoteSystemAdditionalInfoProvider*, uint>)(lpVtbl[1]))((IRemoteSystemAdditionalInfoProvider*)Unsafe.AsPointer(ref this));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [return: NativeTypeName("ULONG")]
+        public uint Release()
+        {
+            return ((delegate* unmanaged<IRemoteSystemAdditionalInfoProvider*, uint>)(lpVtbl[2]))((IRemoteSystemAdditionalInfoProvider*)Unsafe.AsPointer(ref this));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [return: NativeTypeName("HRESULT")]
+        public int GetAdditionalInfo([NativeTypeName("HSTRING *")] IntPtr* deduplicationId, [NativeTypeName("const IID &")] Guid* riid, [NativeTypeName("void **")] void** mapView)
+        {
+            return ((delegate* unmanaged<IRemoteSystemAdditionalInfoProvider*, IntPtr*, Guid*, void**, int>)(lpVtbl[3]))((IRemoteSystemAdditionalInfoProvider*)Unsafe.AsPointer(ref this), deduplicationId, riid, mapView);
+        }
+    }
+}
