fc666b4bac4707c08fb337fcaec2c7fc9757408c
reflectronic <john-tur@outlook.com>
2020-09-27 14:30:15 -0400

    Add UIViewSettingsInterop.h

---
+++ sources/Interop/Windows/um/UIViewSettingsInterop/Windows.cs
@@ -0,0 +1,14 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/UIViewSettingsInterop.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+using System;
+
+namespace TerraFX.Interop
+{
+    public static partial class Windows
+    {
+        public static readonly Guid IID_IUIViewSettingsInterop = new Guid(0x3694DBF9, 0x8F68, 0x44BE, 0x8F, 0xF5, 0x19, 0x5C, 0x98, 0xED, 0xE8, 0xA6);
+    }
+}
