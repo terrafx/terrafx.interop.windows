8d1e37b3eb7fccf4f1f24ca6ee635cde2aa06e26
Tanner Gooding <tagoo@outlook.com>
2020-12-05 22:09:37 -0800

    Updating build to use a NonDesktop solution filter for building on Unix

--- scripts/cibuild.cmd
+++ scripts/cibuild.cmd
@@ -1,5 +1,3 @@
 @ECHO OFF
 powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0build.ps1""" -ci %*"
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0build.ps1""" -ci -solution """%~dp0..\TerraFX.Interop.Windows.Samples.sln""" %*"
 EXIT /B %ERRORLEVEL%
