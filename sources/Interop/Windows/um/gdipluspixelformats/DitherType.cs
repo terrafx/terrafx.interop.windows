fdee43185e7428d13498aa24d2a1fe58ee0b3b22
reflectronic <john-tur@outlook.com>
2021-01-30 20:11:05 -0500

    Add gdipluspixelformats.h

---
+++ sources/Interop/Windows/um/gdipluspixelformats/DitherType.cs
@@ -0,0 +1,22 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/gdipluspixelformats.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+namespace TerraFX.Interop
+{
+    public enum DitherType
+    {
+        DitherTypeNone = 0,
+        DitherTypeSolid = 1,
+        DitherTypeOrdered4x4 = 2,
+        DitherTypeOrdered8x8 = 3,
+        DitherTypeOrdered16x16 = 4,
+        DitherTypeSpiral4x4 = 5,
+        DitherTypeSpiral8x8 = 6,
+        DitherTypeDualSpiral4x4 = 7,
+        DitherTypeDualSpiral8x8 = 8,
+        DitherTypeErrorDiffusion = 9,
+        DitherTypeMax = 10,
+    }
+}
---
+++ sources/Interop/Windows/um/gdipluspixelformats/PaletteType.cs
@@ -0,0 +1,21 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/gdipluspixelformats.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+namespace TerraFX.Interop
+{
+    public enum PaletteType
+    {
+        PaletteTypeCustom = 0,
+        PaletteTypeOptimal = 1,
+        PaletteTypeFixedBW = 2,
+        PaletteTypeFixedHalftone8 = 3,
+        PaletteTypeFixedHalftone27 = 4,
+        PaletteTypeFixedHalftone64 = 5,
+        PaletteTypeFixedHalftone125 = 6,
+        PaletteTypeFixedHalftone216 = 7,
+        PaletteTypeFixedHalftone252 = 8,
+        PaletteTypeFixedHalftone256 = 9,
+    }
+}
