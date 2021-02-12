4e72055b6f6fe80c11a69c41210ca7776340107e
Tanner Gooding <tagoo@outlook.com>
2021-02-11 23:17:13 -0800

    Adding the CreateRootSignature helper to the correct folder

---
+++ sources/Interop/Windows/dxtk12/DirectXHelpers/Windows.Manual.cs
@@ -0,0 +1,29 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from DirectXHelpers.h in the microsoft/DirectXTK12 tag jan2021
+// Original source is Copyright © Microsoft. All rights reserved. Licensed under the University of Illinois Open Source License.
+
+using System;
+using static TerraFX.Interop.D3D_ROOT_SIGNATURE_VERSION;
+
+namespace TerraFX.Interop
+{
+    public static unsafe partial class Windows
+    {
+        [return: NativeTypeName("HRESULT")]
+        public static int CreateRootSignature([NativeTypeName("ID3D12Device *")] ID3D12Device* device, [NativeTypeName("const D3D12_ROOT_SIGNATURE_DESC *")] D3D12_ROOT_SIGNATURE_DESC* rootSignatureDesc, [NativeTypeName("ID3D12RootSignature **")] ID3D12RootSignature** rootSignature)
+        {
+            using ComPtr<ID3DBlob> pSignature = new ComPtr<ID3DBlob>();
+            using ComPtr<ID3DBlob> pError = new ComPtr<ID3DBlob>();
+
+            HRESULT hr = D3D12SerializeRootSignature(rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, pSignature.GetAddressOf(), pError.GetAddressOf());
+
+            if (SUCCEEDED(hr))
+            {
+                hr = device->CreateRootSignature(0, pSignature.Get()->GetBufferPointer(), pSignature.Get()->GetBufferSize(), __uuidof<ID3D12RootSignature>(), (void**)rootSignature);
+            }
+
+            return hr;
+        }
+    }
+}
