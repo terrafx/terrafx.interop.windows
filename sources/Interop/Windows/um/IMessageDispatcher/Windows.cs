2773592c0031fa4268202f2fc5d242b917d87b4d
reflectronic <john-tur@outlook.com>
2021-02-13 20:21:05 -0500

    Add IMessageDispatcher.h

---
+++ sources/Interop/Windows/um/IMessageDispatcher/Windows.cs
@@ -0,0 +1,14 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/IMessageDispatcher.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+using System;
+
+namespace TerraFX.Interop
+{
+    public static partial class Windows
+    {
+        public static readonly Guid IID_IMessageDispatcher = new Guid(0xF5F84C8F, 0xCFD0, 0x4CD6, 0xB6, 0x6B, 0xC5, 0xD2, 0x6F, 0xF1, 0x68, 0x9D);
+    }
+}
