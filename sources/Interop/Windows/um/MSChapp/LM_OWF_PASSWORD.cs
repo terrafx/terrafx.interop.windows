5dd4f50f90b2ff2c35762346c942bc6c782a3131
reflectronic <john-tur@outlook.com>
2021-02-13 21:01:02 -0500

    Add MSChapp.h

---
+++ sources/Interop/Windows/um/MSChapp/LM_OWF_PASSWORD.cs
@@ -0,0 +1,35 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/MSChapp.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace TerraFX.Interop
+{
+    public partial struct LM_OWF_PASSWORD
+    {
+        [NativeTypeName("CYPHER_BLOCK [2]")]
+        public _data_e__FixedBuffer data;
+
+        public partial struct _data_e__FixedBuffer
+        {
+            public CYPHER_BLOCK e0;
+            public CYPHER_BLOCK e1;
+
+            public ref CYPHER_BLOCK this[int index]
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get
+                {
+                    return ref AsSpan()[index];
+                }
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Span<CYPHER_BLOCK> AsSpan() => MemoryMarshal.CreateSpan(ref e0, 2);
+        }
+    }
+}
