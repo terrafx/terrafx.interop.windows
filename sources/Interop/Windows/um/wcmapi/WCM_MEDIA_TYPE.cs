00012a846ef29e996266b824f939e6dc5eda25eb
reflectronic <john-tur@outlook.com>
2021-02-13 23:49:52 -0500

    Add wcmapi.h

---
+++ sources/Interop/Windows/um/wcmapi/WCM_MEDIA_TYPE.cs
@@ -0,0 +1,17 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/wcmapi.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+namespace TerraFX.Interop
+{
+    public enum WCM_MEDIA_TYPE
+    {
+        wcm_media_unknown,
+        wcm_media_ethernet,
+        wcm_media_wlan,
+        wcm_media_mbn,
+        wcm_media_invalid,
+        wcm_media_max,
+    }
+}
