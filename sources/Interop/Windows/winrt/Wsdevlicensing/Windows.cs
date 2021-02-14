d54f16e83914f180149270f3251b21eb0e7c84f1
reflectronic <john-tur@outlook.com>
2021-02-13 20:32:04 -0500

    Add Wsdevlicensing.h

---
+++ sources/Interop/Windows/winrt/Wsdevlicensing/Windows.cs
@@ -0,0 +1,25 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from winrt/Wsdevlicensing.h in the Windows SDK for Windows 10.0.19041.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace TerraFX.Interop
+{
+    public static unsafe partial class Windows
+    {
+        [DllImport("wsclient", ExactSpelling = true)]
+        [return: NativeTypeName("HRESULT")]
+        public static extern int CheckDeveloperLicense([NativeTypeName("FILETIME *")] FILETIME* pExpiration);
+
+        [DllImport("wsclient", ExactSpelling = true)]
+        [return: NativeTypeName("HRESULT")]
+        public static extern int AcquireDeveloperLicense([NativeTypeName("HWND")] IntPtr hwndParent, [NativeTypeName("FILETIME *")] FILETIME* pExpiration);
+
+        [DllImport("wsclient", ExactSpelling = true)]
+        [return: NativeTypeName("HRESULT")]
+        public static extern int RemoveDeveloperLicense([NativeTypeName("HWND")] IntPtr hwndParent);
+    }
+}
