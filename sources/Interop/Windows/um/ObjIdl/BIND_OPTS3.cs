65dbd2fbe29883f2c036a255124d7d48b4dbf0ee
reflectronic <john-tur@outlook.com>
2021-06-24 21:15:29 -0400

    regenerate objidl

--- sources/Interop/Windows/um/ObjIdl/BIND_OPTS3.cs
+++ sources/Interop/Windows/um/ObjIdl/BIND_OPTS3.cs
@@ -1,6 +1,6 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 
-// Ported from um/ObjIdl.h in the Windows SDK for Windows 10.0.20348.0
+// Ported from um/objidl.h in the Windows SDK for Windows 10.0.20348.0
 // Original source is Copyright © Microsoft. All rights reserved.
 
 using System;
@@ -10,7 +10,7 @@ namespace TerraFX.Interop
     [NativeTypeName("struct tagBIND_OPTS3 : tagBIND_OPTS2")]
     public partial struct BIND_OPTS3
     {
-        public BIND_OPTS2 __AnonymousBase_ObjIdl_L9008_C36;
+        public BIND_OPTS2 __AnonymousBase_objidl_L9008_C36;
 
         [NativeTypeName("HWND")]
         public IntPtr hwnd;
