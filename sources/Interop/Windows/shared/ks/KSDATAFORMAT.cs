1bde35f90fd1074eeb2dd47132f73ccde7646696
Tanner Gooding <tagoo@outlook.com>
2021-06-30 20:03:04 -0700

    A few minor fixups for 10.0.20348.0

--- sources/Interop/Windows/shared/ks/KSDATAFORMAT.cs
+++ sources/Interop/Windows/shared/ks/KSDATAFORMAT.cs
@@ -1,6 +1,6 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 
-// Ported from shared/ks.h in the Windows SDK for Windows 10.0.19041.0
+// Ported from shared/ks.h in the Windows SDK for Windows 10.0.20348.0
 // Original source is Copyright © Microsoft. All rights reserved.
 
 using System;
@@ -12,7 +12,7 @@ namespace TerraFX.Interop
     public partial struct KSDATAFORMAT
     {
         [FieldOffset(0)]
-        [NativeTypeName("KSDATAFORMAT::(anonymous struct at C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/shared/ks.h:1096:5)")]
+        [NativeTypeName("KSDATAFORMAT::(anonymous struct at C:/Program Files (x86)/Windows Kits/10/Include/10.0.20348.0/shared/ks.h:1096:5)")]
         internal _Anonymous_e__Struct Anonymous;
 
         public ref uint FormatSize => ref MemoryMarshal.GetReference(MemoryMarshal.CreateSpan(ref Anonymous.FormatSize, 1));
--- sources/Interop/Windows/shared/ks/KSIDENTIFIER.cs
+++ sources/Interop/Windows/shared/ks/KSIDENTIFIER.cs
@@ -1,6 +1,6 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 
-// Ported from shared/ks.h in the Windows SDK for Windows 10.0.19041.0
+// Ported from shared/ks.h in the Windows SDK for Windows 10.0.20348.0
 // Original source is Copyright © Microsoft. All rights reserved.
 
 using System;
@@ -10,7 +10,7 @@ namespace TerraFX.Interop
 {
     public partial struct KSIDENTIFIER
     {
-        [NativeTypeName("KSIDENTIFIER::(anonymous union at C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/shared/ks.h:107:5)")]
+        [NativeTypeName("KSIDENTIFIER::(anonymous union at C:/Program Files (x86)/Windows Kits/10/Include/10.0.20348.0/shared/ks.h:107:5)")]
         internal _Anonymous_e__Union Anonymous;
 
         public ref Guid Set => ref MemoryMarshal.GetReference(MemoryMarshal.CreateSpan(ref Anonymous.Anonymous.Set, 1));
@@ -25,7 +25,7 @@ namespace TerraFX.Interop
         public partial struct _Anonymous_e__Union
         {
             [FieldOffset(0)]
-            [NativeTypeName("KSIDENTIFIER::(anonymous struct at C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/shared/ks.h:111:9)")]
+            [NativeTypeName("KSIDENTIFIER::(anonymous struct at C:/Program Files (x86)/Windows Kits/10/Include/10.0.20348.0/shared/ks.h:111:9)")]
             internal _Anonymous_e__Struct Anonymous;
 
             [FieldOffset(0)]
