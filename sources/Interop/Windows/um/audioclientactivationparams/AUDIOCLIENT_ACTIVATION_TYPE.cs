9819aa5f67002606e6efb95138dff0b71c8ea328
reflectronic <john-tur@outlook.com>
2021-10-10 21:04:47 -0400

    Add audioclientactivationparams.h

---
+++ sources/Interop/Windows/um/audioclientactivationparams/AUDIOCLIENT_ACTIVATION_TYPE.cs
@@ -0,0 +1,13 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/audioclientactivationparams.h in the Windows SDK for Windows 10.0.20348.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+namespace TerraFX.Interop
+{
+    public enum AUDIOCLIENT_ACTIVATION_TYPE
+    {
+        AUDIOCLIENT_ACTIVATION_TYPE_DEFAULT = 0,
+        AUDIOCLIENT_ACTIVATION_TYPE_PROCESS_LOOPBACK = 1,
+    }
+}
