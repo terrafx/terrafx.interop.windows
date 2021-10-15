3c3c57fff5c482401ec7ac02ea6208d31dc6901b
reflectronic <john-tur@outlook.com>
2021-10-14 23:16:52 -0400

    Fix small issues

--- sources/Interop/Windows/um/gdiplusimaging/ImageLockMode.cs
+++ sources/Interop/Windows/um/gdiplusimaging/ImageLockMode.cs
@@ -3,9 +3,11 @@
 // Ported from um/gdiplusimaging.h in the Windows SDK for Windows 10.0.20348.0
 // Original source is Copyright © Microsoft. All rights reserved.
 
+using System;
+
 namespace TerraFX.Interop
 {
-    [Flagss]
+    [Flags]
     public enum ImageLockMode
     {
         ImageLockModeRead = 0x0001,
