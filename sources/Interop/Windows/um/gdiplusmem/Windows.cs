220471612f9a1f8ba02101cf142fa0f260749900
reflectronic <john-tur@outlook.com>
2021-10-14 21:07:31 -0400

    Update gdiplusmem.h

--- sources/Interop/Windows/um/gdiplusmem/Windows.cs
+++ sources/Interop/Windows/um/gdiplusmem/Windows.cs
@@ -1,6 +1,6 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 
-// Ported from um/gdiplusmem.h in the Windows SDK for Windows 10.0.19041.0
+// Ported from um/gdiplusmem.h in the Windows SDK for Windows 10.0.20348.0
 // Original source is Copyright © Microsoft. All rights reserved.
 
 using System.Runtime.InteropServices;
@@ -10,11 +10,10 @@ namespace TerraFX.Interop
     public static unsafe partial class Windows
     {
         [DllImport("gdiplus", ExactSpelling = true)]
-        [return: NativeTypeName("void *")]
         public static extern void* GdipAlloc([NativeTypeName("size_t")] nuint size);
 
         [DllImport("gdiplus", ExactSpelling = true)]
-        public static extern void GdipFree([NativeTypeName("void *")] void* ptr);
+        public static extern void GdipFree(void* ptr);
 
         [NativeTypeName("#define GDIPVER 0x0100")]
         public const int GDIPVER = 0x0100;
