21f88849a7b9e5a14638f9286bebe5fed8af61a3
Tanner Gooding <tagoo@outlook.com>
2021-10-17 20:32:15 -0700

    Generating bindings for um/vssym32.h

---
+++ sources/Interop/Windows/um/vssym32/TRAILINGGRIDCELLUPPERSTATES.cs
@@ -0,0 +1,16 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/vssym32.h in the Windows SDK for Windows 10.0.20348.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+namespace TerraFX.Interop
+{
+    public enum TRAILINGGRIDCELLUPPERSTATES
+    {
+        MCTGCU_HOT = 1,
+        MCTGCU_HASSTATE = 2,
+        MCTGCU_HASSTATEHOT = 3,
+        MCTGCU_SELECTED = 4,
+        MCTGCU_SELECTEDHOT = 5,
+    }
+}
