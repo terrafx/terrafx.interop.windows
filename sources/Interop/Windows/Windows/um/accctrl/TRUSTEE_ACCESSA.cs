ca08daf69c771fe2efcc70a6a52ba220299c5f8d
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2021-12-01 13:49:52 -0800

    Add the primary ACL APIs (AclAPI.h)
    Key thing here is being able to get effective rights. Includes `acccltrl.h` which has the necessary supporting defines.

---
+++ sources/Interop/Windows/Windows/um/accctrl/TRUSTEE_ACCESSA.cs
@@ -0,0 +1,26 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/accctrl.h in the Windows SDK for Windows 10.0.20348.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+namespace TerraFX.Interop.Windows;
+
+/// <include file='TRUSTEE_ACCESSA.xml' path='doc/member[@name="TRUSTEE_ACCESSA"]/*' />
+public unsafe partial struct TRUSTEE_ACCESSA
+{
+    /// <include file='TRUSTEE_ACCESSA.xml' path='doc/member[@name="TRUSTEE_ACCESSA.lpProperty"]/*' />
+    [NativeTypeName("LPSTR")]
+    public sbyte* lpProperty;
+
+    /// <include file='TRUSTEE_ACCESSA.xml' path='doc/member[@name="TRUSTEE_ACCESSA.Access"]/*' />
+    [NativeTypeName("ACCESS_RIGHTS")]
+    public uint Access;
+
+    /// <include file='TRUSTEE_ACCESSA.xml' path='doc/member[@name="TRUSTEE_ACCESSA.fAccessFlags"]/*' />
+    [NativeTypeName("ULONG")]
+    public uint fAccessFlags;
+
+    /// <include file='TRUSTEE_ACCESSA.xml' path='doc/member[@name="TRUSTEE_ACCESSA.fReturnedAccess"]/*' />
+    [NativeTypeName("ULONG")]
+    public uint fReturnedAccess;
+}
