d3b419035444edf047580a42cf5cf3c2b0b68f43
Tanner Gooding <tagoo@outlook.com>
2021-12-23 11:16:00 -0800

    Adding a ThrowIfFailed extension method to HRESULT

---
+++ sources/Interop/Windows/Windows/other/helper-types/Windows.Manual.cs
@@ -0,0 +1,26 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace TerraFX.Interop.Windows;
+
+public static partial class Windows
+{
+    [DoesNotReturn]
+    public static void ThrowExternalException(string methodName, int errorCode)
+    {
+        var message = string.Format("'{0}' failed with an error code of '{1}'", methodName, errorCode);
+        throw new ExternalException(message, errorCode);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfFailed(this HRESULT value, [CallerArgumentExpression("value")] string? valueExpression = null)
+    {
+        if (value.FAILED)
+        {
+            ThrowExternalException(valueExpression ?? "Method", value);
+        }
+    }
+}
