7a5fda7e2999bc7256612cf9b629fa8c5407b2c3
Tanner Gooding <tagoo@outlook.com>
2021-12-24 11:19:51 -0800

    Generating bindings for um/gdiplusmetaheader.h

---
+++ sources/Interop/Windows/Gdiplus/um/gdiplusmetaheader/MetafileHeader.Manual.cs
@@ -0,0 +1,22 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/gdiplusmetaheader.h in the Windows SDK for Windows 10.0.20348.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+using TerraFX.Interop.Windows;
+using static TerraFX.Interop.Gdiplus.MetafileType;
+
+namespace TerraFX.Interop.Gdiplus;
+
+public partial struct MetafileHeader
+{
+    public BOOL IsEmfOrEmfPlus()
+    {
+        return Type >= MetafileTypeEmf;
+    }
+
+    public BOOL IsEmfPlus()
+    {
+        return Type >= MetafileTypeEmfPlusOnly;
+    }
+}
