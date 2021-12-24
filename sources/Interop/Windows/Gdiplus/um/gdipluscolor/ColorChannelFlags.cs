95ccc90a55762fee2568ecc18758295fce69ee18
Tanner Gooding <tagoo@outlook.com>
2021-12-24 11:13:39 -0800

    Generating bindings for um/gdipluscolor.h

---
+++ sources/Interop/Windows/Gdiplus/um/gdipluscolor/ColorChannelFlags.cs
@@ -0,0 +1,25 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/gdipluscolor.h in the Windows SDK for Windows 10.0.20348.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+namespace TerraFX.Interop.Gdiplus;
+
+/// <include file='ColorChannelFlags.xml' path='doc/member[@name="ColorChannelFlags"]/*' />
+public enum ColorChannelFlags
+{
+    /// <include file='ColorChannelFlags.xml' path='doc/member[@name="ColorChannelFlags.ColorChannelFlagsC"]/*' />
+    ColorChannelFlagsC = 0,
+
+    /// <include file='ColorChannelFlags.xml' path='doc/member[@name="ColorChannelFlags.ColorChannelFlagsM"]/*' />
+    ColorChannelFlagsM,
+
+    /// <include file='ColorChannelFlags.xml' path='doc/member[@name="ColorChannelFlags.ColorChannelFlagsY"]/*' />
+    ColorChannelFlagsY,
+
+    /// <include file='ColorChannelFlags.xml' path='doc/member[@name="ColorChannelFlags.ColorChannelFlagsK"]/*' />
+    ColorChannelFlagsK,
+
+    /// <include file='ColorChannelFlags.xml' path='doc/member[@name="ColorChannelFlags.ColorChannelFlagsLast"]/*' />
+    ColorChannelFlagsLast,
+}
