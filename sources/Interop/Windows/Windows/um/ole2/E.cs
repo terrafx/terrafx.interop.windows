b5f7802bf47eee413b7505a1c05e6b3d51f65681
Tanner Gooding <tagoo@outlook.com>
2025-09-07 10:25:04 -0700

    Generate bindings for um/ole2

---
+++ sources/Interop/Windows/Windows/um/ole2/E.cs
@@ -0,0 +1,12 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from um/ole2.h in the Windows SDK for Windows 10.0.26100.0
+// Original source is Copyright © Microsoft. All rights reserved.
+
+namespace TerraFX.Interop.Windows;
+
+public static partial class E
+{
+    [NativeTypeName("#define E_DRAW VIEW_E_DRAW")]
+    public const int E_DRAW = unchecked((int)(0x80040140));
+}
