af718bddc21a178e4c82f926bc92d3cc9d00a046
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-02-23 14:13:18 -0800

    Move DxbcConverter.h to the generation/include folder

---
+++ generation/DirectX/dxilconv/DxbcConverter/dxc/dxcapi.h
@@ -0,0 +1,2 @@
+// DxbcConverter.h includes "dxc/dxcapi.h", so we created this forwarding header to avoid modifying DxbcConverter.h.
+#include <dxcapi.h>
--- generation/include/dxilconv/DxbcConverter.h
+++ generation/include/dxilconv/DxbcConverter.h
@@ -12,8 +12,7 @@
 #ifndef __DXBC_CONVERTER__H__
 #define __DXBC_CONVERTER__H__
 
-#include "..\..\..\TerraFX.h"
-#include <dxcapi.h>
+#include "dxc/dxcapi.h"
 
 #ifndef _MSC_VER
 extern "C"
